cdb0ee23998465391ea74677ef5e3eb68926bc02
ImadMachi <machii.imad@gmail.com>
2023-10-28 14:25:28 +0100

    add cloudinary keys

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,72 +5,59 @@ import { openMenu } from "@/redux/slices/MenuSlice";
 import Search from "./Search";
 
 import { Bars3Icon } from "@heroicons/react/24/outline";
-import amazonLogo from "../../public/assets/images/amazon-logo.png";
+import Logo from "../../public/assets/images/logo.png";
 import AccountButtons from "./AccountButtons";
 import Language from "./Language";
 import HeaderBottom from "./HeaderBottom";
 import DeliveryTo from "./DeliveryTo";
 import Link from "next/link";
 
-const Header = ({title, searchHandler}: any) => {
-    const dispatch = useAppDispatch();
+const Header = ({ title, searchHandler }: any) => {
+	const dispatch = useAppDispatch();
 
-    const openMenuHandler = () => {
-        dispatch(openMenu());
-    };
+	const openMenuHandler = () => {
+		dispatch(openMenu());
+	};
 
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content="full amazon clone React" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-            </Head>
-            <header>
-                <div className="bg-amazon-blue_dark flex flex-col md:flex-row">
-                    <div className="flex flex-grow items-center p-3 md:space-x-5 md:px-4 text-white">
-                        {/* Menu Icon Mobile */}
-                        <div className="flex items-center justify-center">
-                            <div
-                                onClick={openMenuHandler}
-                                className="md:hidden cursor-pointer mr-1"
-                            >
-                                <Bars3Icon className="h-8 md:h-7" />
-                            </div>
-                            {/* Logo */}
-                            <Link href="/">
-                                <Image
-                                    src={amazonLogo}
-                                    alt="amazon-logo"
-                                    className="object-contain w-20 md:w-28 pt-2"
-                                />
-                            </Link>
-                        </div>
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta name="description" content="Giftify test" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<header>
+				<div className="bg-amazon-blue_dark flex flex-col md:flex-row">
+					<div className="flex flex-grow items-center p-3 md:space-x-5 md:px-4 text-white">
+						{/* Menu Icon Mobile */}
+						<div className="flex items-center justify-center">
+							<div onClick={openMenuHandler} className="md:hidden cursor-pointer mr-1">
+								<Bars3Icon className="h-8 md:h-7" />
+							</div>
+							{/* Logo */}
+							<Link href="/" className="font-bold text-2xl">
+								<Image src={Logo} alt="logo" className="w-40" />
+							</Link>
+						</div>
 
-                        <DeliveryTo />
+						{/* Search Desktop*/}
+						<div className="hidden md:flex flex-grow">
+							<Search searchHandler={searchHandler} />
+						</div>
 
-                        {/* Search Desktop*/}
-                        <div className="hidden md:flex flex-grow">
-                            <Search searchHandler={searchHandler} />
-                        </div>
+						<AccountButtons />
+					</div>
 
-                        <Language />
-                        <AccountButtons />
-                    </div>
+					{/* Search Mobile*/}
+					<div className="md:hidden">
+						<Search />
+					</div>
+				</div>
 
-                    {/* Search Mobile*/}
-                    <div className="md:hidden">
-                        <Search />
-                    </div>
-                </div>
-
-                <HeaderBottom handleOpenMenu={openMenuHandler} />
-            </header>
-        </>
-    );
+				<HeaderBottom handleOpenMenu={openMenuHandler} />
+			</header>
+		</>
+	);
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,35 +9,32 @@ import CategoriesProduct from "@/components/Home/CategoriesProduct/CategoriesPro
 import db from "../utils/db";
 
 export default function Home({ products }: any) {
-    return (
-        <>
-            <Header title="Full Amazon Clone React" />
-            <main className="max-w-screen-2xl mx-auto bg-gray-100">
-                <CarouselContainer />
-                <CategoriesProduct products={products} />
-                <div className="z-10 relative">
-                    <HomeProductSwiper products={products} category="women clothing" />
-                    <HomeProductSwiper products={products} category="shoes" />
-                    <HomeProductSwiper products={products} category="Beauty" />
-                    <HomeProductSwiper products={products} category="Kids" />
-                </div>
-            </main>
-            <Footer />
-            <MenuSideBar />
-        </>
-    );
+	return (
+		<>
+			<Header title="Full Amazon Clone React" />
+			<main className="max-w-screen-2xl mx-auto bg-gray-100">
+				<CarouselContainer />
+				<CategoriesProduct products={products} />
+				<div className="z-10 relative">
+					<HomeProductSwiper products={products} category="women clothing" />
+					<HomeProductSwiper products={products} category="shoes" />
+					<HomeProductSwiper products={products} category="Beauty" />
+					<HomeProductSwiper products={products} category="Kids" />
+				</div>
+			</main>
+			<Footer />
+			<MenuSideBar />
+		</>
+	);
 }
 
 export const getServerSideProps = async (context: any) => {
-    db.connectDb();
-    const products = await Product.find()
-        .populate({ path: "category", model: Category })
-        .sort({ updatedAt: -1 })
-        .lean();
-    db.disconnectDb();
-    return {
-        props: {
-            products: JSON.parse(JSON.stringify(products)),
-        },
-    };
+	db.connectDb();
+	const products = await Product.find().populate({ path: "category", model: Category }).sort({ updatedAt: -1 }).lean();
+	db.disconnectDb();
+	return {
+		props: {
+			products: JSON.parse(JSON.stringify(products)),
+		},
+	};
 };
